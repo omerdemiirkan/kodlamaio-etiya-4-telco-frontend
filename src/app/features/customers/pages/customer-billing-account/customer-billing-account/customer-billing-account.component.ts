6a99bafd1a4f0b7f34a94035a7de58c2c2cf6f96
ÖMER DEMİRKAN <63349869+omerdemiirkan@users.noreply.github.com>
2022-09-11 02:32:17 +0300

    customer-billing-account refactor

--- src/app/features/customers/pages/customer-billing-account/customer-billing-account/customer-billing-account.component.ts
+++ src/app/features/customers/pages/customer-billing-account/customer-billing-account/customer-billing-account.component.ts
@@ -7,7 +7,7 @@ import { CustomersService } from '../../../services/customer/customers.service';
 import { Address } from '../../../models/address';
 import { Customer } from '../../../models/customer';
 import { BillingAccount } from '../../../models/billingAccount';
-import { MessageService } from 'primeng/api';
+
 
 @Component({
   templateUrl: './customer-billing-account.component.html',
@@ -21,26 +21,22 @@ export class CustomerBillingAccountComponent implements OnInit {
   selectedCustomerId!: number;
   customer!: Customer;
   billingAccount!: BillingAccount;
+  isValid: boolean = false;
+  isShownError: boolean = false;
 
   billingAdress: Address[] = [];
-  addresses!: Address;
-  mainAddres!: number;
 
   constructor(
     private formBuilder: FormBuilder,
     private cityService: CityService,
     private customerService: CustomersService,
     private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private messageService: MessageService
+    private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     this.getParams();
     this.getCityList();
-    this.getMainAddress();
-    this.createAddressForm();
-    this.createAccountForm();
   }
 
   getParams() {
@@ -58,6 +54,8 @@ export class CustomerBillingAccountComponent implements OnInit {
         .getCustomerById(this.selectedCustomerId)
         .subscribe((data) => {
           this.customer = data;
+          this.createAddressForm();
+          this.createAccountForm();
         });
     }
   }
@@ -65,7 +63,7 @@ export class CustomerBillingAccountComponent implements OnInit {
   createAccountForm() {
     this.accountForm = this.formBuilder.group({
       accountName: ['', Validators.required],
-      accountDescription: ['', Validators.required],
+      description: ['', Validators.required],
     });
   }
 
@@ -90,81 +88,52 @@ export class CustomerBillingAccountComponent implements OnInit {
     });
   }
 
-  isMainAdd() {
-    return this.addresses == undefined ? true : false;
-  }
-
   addAddress() {
-    const addressToAdd: Address = {
-      ...this.addressForm.value,
-      city: this.cityList.find(
-        (city) => city.id == this.addressForm.value.city.id
-      ),
-      isMain: this.isMainAdd(),
-    };
-    this.billingAdress.push(addressToAdd);
-    console.log(this.billingAdress);
-    this.isShown = false;
+    if (this.addressForm.valid) {
+      this.isShownError = false;
+      const addressToAdd: Address = {
+        ...this.addressForm.value,
+        city: this.cityList.find(
+          (city) => city.id == this.addressForm.value.city
+        ),
+      };
+      this.billingAdress.push(addressToAdd);
+      console.log(addressToAdd);
+      this.isShown = false;
+    } else {
+      this.isValid = false;
+      this.isShownError = true;
+    }
   }
 
   add() {
-    //this.billingAccount = this.accountForm.value;
-    //this.billingAccount.addresses = this.billingAdress;
-    let newBillingAccount: BillingAccount = {
-      ...this.accountForm.value,
-      addresses: [...this.billingAdress, this.addresses],
-    };
-    this.customerService
-      .addBillingAccount(newBillingAccount, this.customer)
-      .subscribe({
-        next: () => {
-          this.messageService.add({
-            detail: 'Sucsessfully added',
-            severity: 'success',
-            summary: 'Add',
-            key: 'etiya-custom',
-          });
+    if (this.accountForm.valid) {
+      this.isValid = false;
+      this.billingAccount = this.accountForm.value;
+      this.billingAccount.addresses = this.billingAdress;
+      this.billingAccount.status = 'active';
+      this.billingAccount.accountNumber = String(
+        Math.floor(Math.random() * 1000000000)
+      );
+      console.log(this.billingAccount);
+      this.customerService
+        .addBillingAccount(this.billingAccount, this.customer)
+        .subscribe(() => {
           this.router.navigateByUrl(
-            `/dashboard/customers/customer-billing-account-detail/${this.selectedCustomerId}`
+            '/dashboard/customers/customer-billing-account-detail/' +
+              this.selectedCustomerId
           );
-        },
-        error: (err) => {
-          this.messageService.add({
-            detail: 'Error created',
-            severity: 'danger',
-            summary: 'Error',
-            key: 'etiya-custom',
-          });
-        },
-      });
-  }
-  getMainAddress() {
-    this.customerService
-      .getCustomerById(this.selectedCustomerId)
-      .subscribe((data) => {
-        data.addresses?.forEach((adr) => {
-          if (adr.isMain == true) this.addresses = adr;
         });
-      });
+    } else {
+      this.isShownError = false;
+      this.isValid = true;
+    }
   }
-  handleConfigInput(event: any) {
-    this.mainAddres = event.target.value;
-    //this.add(event.target.value)
-    this.billingAccount.addresses = this.billingAccount.addresses?.map(
-      (adr) => {
-        const newAddress = { ...adr, isMain: false };
-        return newAddress;
-      }
+
+  goToPreviousPage() {
+    this.router.navigateByUrl(
+      '/dashboard/customers/customer-billing-account-detail/' +
+        this.selectedCustomerId
     );
-
-    let findAddressBill = this.billingAccount.addresses.find((adr) => {
-      return adr.id == event.target.value;
-    });
-
-    findAddressBill!.isMain = true;
-    this.customerService.update(this.customer).subscribe((data) => {
-      console.log(data);
-      this.getCustomerById();
-    });
   }
 }
